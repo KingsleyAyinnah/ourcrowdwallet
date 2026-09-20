1c41c2431a680621463e55a5dd4bb8a40660750e
Kingsley Ayinnah <kingsleyayinnah@gmail.com>
2026-09-20 18:32:32 +0100

    Update config ignore rules

--- config/database.php
+++ config/database.php
@@ -24,9 +24,9 @@ if ($user === null || $pass === null || $dbname === null) {
 
     if ($is_live) {
         // Live cPanel Default Fallbacks (if no .env is created)
-        $dbname = $dbname ?? 'ourcoldh_ourcr';
-        $user   = $user   ?? 'ourcoldh_ourcr';
-        $pass   = $pass   ?? '8~*}Aw3S[QHQ[-e2';
+        $dbname = $dbname ?? '';
+        $user   = $user   ?? '';
+        $pass   = $pass   ?? '';
     } else {
         // Localhost Default Fallbacks
         $dbname = $dbname ?? 'ourcr_online';
